681fffba5742fbbbacacaefaf4ee53a8b17ee1d3
Amulya Jain <amulyajain123@gmail.com>
2024-02-29 04:44:32 +0530

    background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
     fontFamily: {
       sans: ['Inter','sans-serif'],
       kode: ['Kode Mono', 'sans-serif'],
+      poppins: ['Poppins','sans-serif'],
+      mont:['Montserrat','sans-serif'],
     }, 
     extend: {},
   },
   safelist: {
-    
+
   },
   plugins: [],
 }

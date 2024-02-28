a207cd97e50620b0e1bfee0ab6c7956dd2c42e98
Amulya Jain <amulyajain123@gmail.com>
2024-02-28 21:59:20 +0530

    first_commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let seeMore=document.querySelector('#see-more');
+seeMore.addEventListener('click',() =>{
+    let hiddenPart=document.querySelector('#hidden-top-sidebar');
+    hiddenPart
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter','sans-serif'],
+      kode: ['Kode Mono', 'sans-serif'],
+    }, 
+    extend: {},
+  },
+  plugins: [],
+}
+
